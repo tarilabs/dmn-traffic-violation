1007d70e3a90c5512d7fef462bf06f42df5a5886
Salifou Sidi M. Malick <ssidimah@redhat.com>
2020-02-13 11:41:19 -0500

    Added Driver Pojo

---
+++ src/main/java/com/github/salifou/dmn_traffic_violation/Driver.java
@@ -0,0 +1,23 @@
+package com.github.salifou.dmn_traffic_violation;
+
+/**
+ * Driver
+ */
+public class Driver {
+
+    private int Points;
+
+    /**
+     * @return the points
+     */
+    public int getPoints() {
+        return Points;
+    }
+
+    /**
+     * @param points the points to set
+     */
+    public void setPoints(int points) {
+        Points = points;
+    }
+}
